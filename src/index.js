8e385464290127fbea006cb36612850dcb9f8393
Erik Medeiros <erikmedeiros4@gmail.com>
2023-11-24 19:16:38 -0300

    fetch tags attributes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import { JSDOM } from "jsdom";
-import https from "node:https";
 
 const MOZILLA_MAIN_URL = new URL("https://developer.mozilla.org/en-US/docs/Web/HTML/Element");
-https.get(MOZILLA_MAIN_URL, (response) => {
-  let data = "";
-  response.on("data", (d) => (data += d));
-  response.on("end", () => console.log(parseMainPage(new JSDOM(data).window.document)));
-});
+const html = await (await fetch(MOZILLA_MAIN_URL)).text();
+const tagsMetadata = parseMainPage(new JSDOM(html).window.document);
+const tagsDataResult = await Promise.allSettled(tagsMetadata.map(parseTagPage));
+
+for (const tagData of tagsDataResult) {
+  switch (tagData.status) {
+    case "fulfilled":
+      process.stdout.write(JSON.stringify(tagData.value, null, 2) + "\n", "utf8");
+      break;
+    case "rejected":
+      /** @type {string}  */
+      let error;
+
+      if (tagData.reason instanceof Error) {
+        const { message, name, stack } = tagData.reason;
+        error = JSON.stringify({ name: name, message: message, stack: stack }, null, 2);
+      } else {
+        error = tagData.reason.toString();
+      }
+
+      process.stderr.write(error + "\n", "utf8");
+      break;
+  }
+}
 
 /**
  * @typedef TagMetadata
@@ -31,6 +49,35 @@ function parseMainPage(document) {
       const url = href ? new URL(href, MOZILLA_MAIN_URL.origin) : null;
       const description = tr.children[1].textContent;
       output.push({ name: td.textContent.slice(1, -1), url, description });
+    }
+  }
+
+  return output;
+}
+
+/**
+ * @typedef TagData
+ * @property {string} name
+ * @property {string} description
+ * @property {string[]} attributes
+ */
+
+/**
+ * @param {TagMetadata} metadata
+ * @returns {Promise<TagData>}
+ */
+async function parseTagPage(metadata) {
+  /** @type {TagData}  */
+  const output = { name: metadata.name, description: metadata.description, attributes: [] };
+  if (!metadata.url) Promise.resolve(output);
+
+  const html = await (await fetch(metadata.url)).text();
+  const document = new JSDOM(html).window.document;
+  const dls = document.querySelectorAll("div > dl");
+
+  for (const dl of dls) {
+    for (const attribute of dl.getElementsByTagName("dt")) {
+      output.attributes.push(attribute.textContent);
     }
   }
 
